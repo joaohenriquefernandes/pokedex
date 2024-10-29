2b2020e9b0e4b5868844e68054a19059fc602031
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-10-29 10:37:22 -0300

    feat: create header component

---
+++ src/views/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="bg-red-600 w-full h-16 flex items-center justify-center">
+      <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="pokeapi-logo" className="w-[207px] h-[53px]"/>
+    </header>
+  )
+}
