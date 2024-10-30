1ba8f9a533d66db55ccf066701ae6170350d18a1
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-10-29 21:55:40 -0300

    feat: create pokemons interface

---
+++ src/app/interfaces/pokemons.ts
@@ -0,0 +1,125 @@
+export interface IPokemons {
+  abilities: {
+    ability: {
+      name: string;
+      url: string;
+    };
+    is_hidden: false;
+    slot: number;
+  }[];
+  base_experience: number;
+  cries: {
+    latest: string;
+    legacy: string;
+  };
+  forms: {
+    name: string;
+    url: string;
+  }[];
+  game_indices: {
+    game_index: number;
+    version: {
+      name: string;
+      url: string;
+    };
+  }[];
+  height: 7;
+  held_items: {
+    item: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      rarity: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: {
+    move: {
+      name: string;
+      url: string;
+    };
+    version_group_details: {
+      level_learned_at: number;
+      move_learn_method: {
+        name: string;
+        url: string;
+      };
+      version_group: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+  name: string;
+  order: number;
+  past_abilities: [];
+  past_types: {
+    generation: {
+      name: string;
+      url: string;
+    };
+  }[];
+  species: {
+    name: string;
+    url: string;
+  };
+  sprites: {
+    back_default: string;
+    back_female: string | null;
+    back_shiny: string | null;
+    back_shiny_female: string | null;
+    front_default: string;
+    front_female: string | null;
+    front_shiny: string | null;
+    front_shiny_female: string | null;
+    other: {
+      dream_world: {
+        front_default: string;
+        front_female: string | null;
+      };
+      home: {
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string | null;
+        front_shiny_female: string | null;
+      };
+      'official-artwork': {
+        front_default: string;
+        front_shiny: string | null;
+      };
+      showdown: {
+        back_default: string;
+        back_female: string | null;
+        back_shiny: string | null;
+        back_shiny_female: string | null;
+        front_default: string;
+        front_female: string | null;
+        front_shiny: string | null;
+        front_shiny_female: string | null;
+      };
+    };
+  };
+  stats: {
+    base_stat: number;
+    effort: number;
+    stat: {
+      name: string;
+      url: string;
+    };
+  }[];
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
+  weight: number;
+}
