e2cb50e7ee181ebd00707f5f6b261ee76d0c37b0
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-10-30 22:52:34 -0300

    fix: rename file and create router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+import { queryClient } from './app/lib/react-query';
+import { router } from './routes';
+import './views/styles/index.css';
+
 export function App() {
   return (
-    <h1>Hello</h1>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
