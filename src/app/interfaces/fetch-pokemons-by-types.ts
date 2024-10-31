d7176ffebda044630afdee3b8982af3c4f5d9d48
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-10-30 22:45:36 -0300

    feat: create fetch pokemons by types interface

---
+++ src/app/interfaces/fetch-pokemons-by-types.ts
@@ -0,0 +1,48 @@
+export interface IFetchPokemonsByTypes {
+  damage_relations: {
+    double_damage_from: {
+      name: string;
+      url: string;
+    }[];
+    double_damage_to: {
+      name: string;
+      url: string;
+    }[];
+    half_damage_from: {
+      name: string;
+      url: string;
+    }[];
+    half_damage_to: {
+      name: string;
+      url: string;
+    }[];
+    no_damage_from: {
+      name: string;
+      url: string;
+    }[];
+    no_damage_to: {
+      name: string;
+      url: string;
+    }[];
+  };
+  game_indices: {
+    game_index: number;
+    generation: {
+      name: string;
+      url: string;
+    };
+  }[];
+  generation: {
+    name: string;
+    url: string;
+  };
+  id: number;
+  name: string;
+  pokemon: {
+    pokemon: {
+      name: string;
+      url: string;
+    };
+    slot: number;
+  }[];
+}
