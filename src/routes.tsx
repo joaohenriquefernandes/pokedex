8775d3c9bbe15d0862f78476cdfea405bfaf6d69
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-10-30 22:55:58 -0300

    feat: define main route

---
+++ src/routes.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Home } from './views/pages/home';
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+]);
